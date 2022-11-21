7fb4004bcaf1703ca801953edc087892da437171
ZheikoN <negru.inger@gmail.com>
2022-11-21 23:39:02 +0000

    add posting for users functionality

--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Thread
+from .forms import PostForm
 
 
 class ThreadList(generic.ListView):
@@ -15,7 +16,7 @@ class ThreadDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Thread.objects.filter(status=1)
         thread = get_object_or_404(queryset, slug=slug)
-        post = thread.posts.filter(approved=True).order_by('-created_on')
+        posts = thread.posts.filter(approved=True).order_by('-created_on')
 
         upvotes = False
         if thread.upvotes.filter(id=self.request.user.id).exists():
@@ -25,11 +26,45 @@ class ThreadDetail(View):
             downvotes = True
 
         return render(
-            request, "post_detail.html",
+            request, "thread_detail.html",
             {
                 "thread": thread,
-                "post": post,
+                "posts": posts,
                 "upvotes": upvotes,
-                "downvotes": downvotes
+                "downvotes": downvotes,
+                "post_form": PostForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Thread.objects.filter(status=1)
+        thread = get_object_or_404(queryset, slug=slug)
+        posts = thread.posts.filter(approved=True).order_by('-created_on')
+
+        upvotes = False
+        if thread.upvotes.filter(id=self.request.user.id).exists():
+            upvotes = True
+        downvotes = False
+        if thread.downvotes.filter(id=self.request.user.id).exists():
+            downvotes = True
+
+        post_form = PostForm(data=request.POST)
+        if post_form.is_valid():
+            post_form.instance.email = request.user.email
+            post_form.instance.name = request.user.username
+            post = post_form.save(commit=False)
+            post.thread = thread
+            post.save()
+        else:
+            post_form = PostForm()
+
+        return render(
+            request, "thread_detail.html",
+            {
+                "thread": thread,
+                "posts": posts,
+                "post_form": PostForm,
+                "upvotes": upvotes,
+                "downvotes": downvotes,
             },
         )
