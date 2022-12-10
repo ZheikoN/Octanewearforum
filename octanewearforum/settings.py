2f08fb08a37c1c1afc814a8f755bab8d3a1c4b5b
ZheikoN <negru.inger@gmail.com>
2022-12-10 19:43:51 +0000

    styled base.html

--- octanewearforum/settings.py
+++ octanewearforum/settings.py
@@ -158,6 +158,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
